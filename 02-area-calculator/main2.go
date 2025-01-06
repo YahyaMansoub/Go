7f7e1aafec51daddf8eb8ed008b9eb2b194d6c2b
YahyaMansoub <yahyamansoub20@gmail.com>
2025-01-06 11:15:32 +0100

    walla

---
+++ 02-area-calculator/main2.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+
+
+func main(){
+	x,y  := 45.0,18.0
+	fmt.Printf("the area of the rectangle is %f\n",x*y)
+	fmt.Printf("the area of the rectangle is %f\n",x*y*3.28)
+
+}
