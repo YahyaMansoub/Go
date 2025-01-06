083cedacce338d89119363f31ac6a6c503f3535f
YahyaMansoub <yahyamansoub20@gmail.com>
2025-01-06 15:27:44 +0100

    loops nerfed

---
+++ exercice3/main.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+
+
+func checkgrade(grade float64){
+	if grade >= 90 {
+			fmt.Println("Grade 1 got : A ,",grade)
+		}else if grade >= 80 && grade<90 {
+			fmt.Println("Grade 1 got : B ,",grade)
+		}else if grade >= 70 && grade<80 {
+			fmt.Println("Grade 1 got : C ,",grade)
+		}else if grade >= 60 && grade<70 {
+			fmt.Println("Grade 1 got : D ,",grade)
+		}else if grade >= 90 {
+			fmt.Println("Grade 1 got : A ,",grade)
+		}else if grade >= 0 && grade<60 {
+			fmt.Println("Grade 1 got : F ,",grade)
+		}
+
+}
+
+func main(){
+
+	// first assignement1
+	var grade [5]float64
+	
+
+	for i :=0; i <5; i++{
+
+		var input float64
+
+        
+		fmt.Println("Enter Grade ",i+1)
+		fmt.Scan(&input)
+
+		if grade[i]<=100 && grade[i]>=0{
+
+			grade[i]=input
+
+		}else{
+			fmt.Println("Invalid grade, retry again :")
+			i--
+		}
+
+	}
+	// second assignment2
+	for i :=0; i<5; i++{
+
+		checkgrade(grade[i])
+
+	}
+	// third assignement3
+
+	var quit bool = false
+	
+	for !quit{
+		var choice string
+		var input float64
+
+		fmt.Println("Do you want to continue(Yes(Y)/No(N))?")
+		fmt.Println("-->")78
+
+		fmt.Scan(&choice)
+		if choice == "Y" || choice == "y" {
+			fmt.Println("Enter Grade")
+			fmt.Println("-->")
+			fmt.Scan(&input)
+			checkgrade(input)
+		}else if choice == "N" || choice == "n" {
+			quit = true
+		}else{
+			fmt.Println("Write an adequate prompt (Y/N) or (y/n) please.")
+		}
+
+	}
+
+
+
+}
---
+++ loops/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var sum int = 1
+	for sum < 100 {
+		fmt.Println("Sum is still under budget")
+		sum *=10
+	}
+
+	numbers := []int{1,2,3}
+	for index,value := range numbers {
+		fmt.Println(index,value)
+	}
+}
