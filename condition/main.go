7264518a4eaf4de752d26e694144980c59a89dd9
YahyaMansoub <yahyamansoub20@gmail.com>
2025-01-06 14:24:51 +0100

    uhm

--- condition/main.go
+++ condition/main.go
@@ -3,19 +3,51 @@ package main
 import "fmt"
 
 func main(){
-	var length float64
-	var width float64
+	var length float64 = 0
+	var width float64 = 0
 
 
 	if length == 0 {
 		fmt.Println("length empty")
-	}
-	else if width == 0 {
+	}else if width == 0 {
 		fmt.Println("width empty")
-	}
-	else if isLengthEmpty:= length == 0; isLengthEmpty{  //the variable is valable only within the if statement , undefined out fo the if statement 
+	}else if isLengthEmpty:= length == 0; isLengthEmpty{  //the variable is valable only within the if statement , undefined out fo the if statement 
 		fmt.Println("length empty")  
 
 	}
+
+	grade := "A"
+	switch grade {
+
+	case "A":
+		fmt.Println("You got an A!")
+		fallthrough   //fallthrough : makes it possible to excecute the conditions below 
+	// in Go ,  go automaticaally breaks 
+	case "B":
+		fmt.Println("You got an A!")
+	
+    default:
+		fmt.Println("You got an A!")
+	}
+
+	number :=9
+
+
+	switch {
+	case number < 10:
+		fmt.Println("less than 10")
+		fallthrough
+	case number < 20:
+		fmt.Println("less than 20")
+		fallthrough
+	case number < 30:
+		fmt.Println("less than 30")
+		fallthrough
+	default:
+		fmt.Println("default case")
+	}
+
+
+
 
 }
