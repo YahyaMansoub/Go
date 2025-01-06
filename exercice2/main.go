983c4f84fb11181b03e67e36be292cfdd82053ed
YahyaMansoub <yahyamansoub20@gmail.com>
2025-01-06 10:54:42 +0100

    ehm

--- exercice2/main.go
+++ exercice2/main.go
@@ -1,11 +1,19 @@
 package main
 
 import "fmt"
+
+const pi float64 = 3.14
+
 func main(){
 	var x,y int
 	x=10
 	y=20
 	var name="Golang"
-	fmt.Printf("x = %T, y = %d, %s",x,y,name)
+	fmt.Printf("x = %d, y = %d, %s\n",x,y,name)
+
+	x:=10
+
+	const pi float64 = 3
+	fmt.Println(pi)
 
 }
