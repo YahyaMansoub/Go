63f63870c10f11cc75d275650c02ef1798a84454
YahyaMansoub <yahyamansoub20@gmail.com>
2025-01-09 14:49:06 +0100

    Concurrency exercice

---
+++ day4/ConcureentNumbers/waitGroups.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func GenSlic() []int {
+	var slice []int
+	for i := 0; i < 10; i++ {
+		slice = append(slice, i+1)
+	}
+	return slice
+}
+
+func squareNumber(number int, wg *sync.WaitGroup) {
+	defer wg.Done() 
+	result := number * number
+	fmt.Println("Square of", number, "is", result)
+}
+
+func main() {
+	slice := GenSlic()
+	var wg sync.WaitGroup 
+
+	for _, value := range slice {
+		wg.Add(1) 
+		go squareNumber(value, &wg) 
+	}
+
+	wg.Wait() 
+}
